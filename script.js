a134eabd6e956246dd1a433cf954369adeba5cb3
Eligio <eligioestevez@hotmail.com>
2023-05-07 11:19:39 -0400

    parallax

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
-//alert("hola mundo");
+window.onscroll = function(){
+    var multiplicador_parallax = 0.2;
+
+    var posicion = window.pageYOffset || document.documentElement.scrollTop;
+    var img_header = document.getElementById("img-header");
+
+    img_header.style.bottom = posicion * multiplicador_parallax + "px"
+}
